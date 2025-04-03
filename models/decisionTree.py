1144dbbe9ffe05f5315ecbc5fa828fa166883b75
Rishabh Seth <rishabh.seth@torontomu.ca>
2025-04-03 12:23:58 -0500

    Update decisionTree.py

--- models/decisionTree.py
+++ models/decisionTree.py
@@ -1,9 +1,8 @@
 import pandas as pd
+import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score, f1_score, precision_score, confusion_matrix, recall_score
-from sklearn.datasets import make_classification
-
 
 # Step 1: Import dataset
 data = pd.read_csv('cancerDataset.csv')
@@ -25,46 +24,46 @@ data['LUNG_CANCER'] = data['LUNG_CANCER'].replace(['YES', 'NO'], [1,0])
 X = data.drop(columns=['LUNG_CANCER'])
 y = data.LUNG_CANCER
 
-#X, y = make_classification(n_samples=500, n_features=5, random_state=42)
-
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
-myTree = DecisionTreeClassifier()
+myTree = DecisionTreeClassifier(random_state=42)
 
 myTree = myTree.fit(X_train, y_train)
 
 y_pred = myTree.predict(X_test)
 
+yTrain = myTree.predict(X_train)
+
 # Step 4: Evaluating Metrics
+
+accuracyTrain = accuracy_score(y_train, yTrain)
+
+print("Accuracy on training set: ", round(accuracyTrain, 4)*100)
+
 accuracy = accuracy_score(y_test, y_pred)
 f1  = f1_score(y_test, y_pred, average = 'weighted')
 precision = precision_score(y_test, y_pred, average = 'weighted')
 recall = recall_score(y_test, y_pred, average = 'weighted')
 confusionMatrix = confusion_matrix(y_test, y_pred)
 
-print("Accuracy on test set:", accuracy)
-print("F1 on test set:", f1)
-print("Precision on test set:", precision)
-print("Recall on test set:", recall)
+
+print("Accuracy on test set:", round(accuracy,3)*100,"%")
+print("F1 on test set:", round(f1,3)*100,"%")
+print("Precision on test set:", round(precision,3)*100,"%")
+print("Recall on test set:", round(recall, 3)*100,"%")
 print("Confusion Matrix on test set:\n", confusionMatrix)
 
 
-"""
 # K-Fold Cross-Validation
-kf = KFold(n_splits=10, shuffle=True, random_state=42)
 
-# Perform Cross-Validation
-accuracy_cv = cross_val_score(myTree, X, y, cv=kf, scoring='accuracy')
-f1_cv = cross_val_score(myTree, X, y, cv=kf, scoring='f1_weighted')
-precision_cv = cross_val_score(myTree, X, y, cv=kf, scoring='precision_weighted')
-recall_cv = cross_val_score(myTree, X, y, cv=kf, scoring='recall_weighted')
+accuracy_cv = cross_val_score(myTree, X, y, cv=10, scoring='accuracy')
+f1_cv = cross_val_score(myTree, X, y, cv=10, scoring='f1_weighted')
+precision_cv = cross_val_score(myTree, X, y, cv=10, scoring='precision_weighted')
+recall_cv = cross_val_score(myTree, X, y, cv=10, scoring='recall_weighted')
 
-# Display Mean Cross-Validation Scores
-print(f"Cross-Validation Accuracy: {np.mean(accuracy_cv):.4f}")
-print(f"Cross-Validation F1 Score: {np.mean(f1_cv):.4f}")
-print(f"Cross-Validation Precision: {np.mean(precision_cv):.4f}")
-print(f"Cross-Validation Recall: {np.mean(recall_cv):.4f}")
-
-"""
+print(f"Cross-Validation Accuracy: {np.mean(accuracy_cv*100):.4f}%")
+print(f"Cross-Validation F1 Score: {np.mean(f1_cv*100):.4f}%")
+print(f"Cross-Validation Precision: {np.mean(precision_cv*100):.4f}%")
+print(f"Cross-Validation Recall: {np.mean(recall_cv*100):.4f}%")
  
