3d1ed5b83faa13c76fd898cee99765313aa6db87
screyes-tmu <samuelluigi.reyes@torontomu.ca>
2025-04-04 22:12:28 -0400

    Update nearestNeighbor.py
    Added 4-fold cross validation results for trained data.

--- models/nearestNeighbor.py
+++ models/nearestNeighbor.py
@@ -11,6 +11,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv('dataset.csv', header=None, skiprows=lambda x: x in [0, 1, 2, ...])
 data.columns = [
@@ -38,20 +39,37 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.3,
                                                     random_state=0)
 
-nbrs = KNeighborsClassifier(n_neighbors=5)
-nbrs.fit(X_train, y_train)
+scaler = StandardScaler()
+X_trainS = scaler.fit_transform(X_train)
+X_testS = scaler.transform(X_test)
 
-aScore = cross_val_score(nbrs, X, y, cv=10, scoring='accuracy')
-fScore = cross_val_score(nbrs, X, y, cv=10, scoring='f1')
-pScore = cross_val_score(nbrs, X, y, cv=10, scoring='precision')
-rScore = cross_val_score(nbrs, X, y, cv=10, scoring='recall')
+nbrs = KNeighborsClassifier(n_neighbors=5)
+nbrs.fit(X_trainS, y_train)
+
+y_pred = nbrs.predict(X_testS)
+XS = scaler.fit_transform(X)
+
+aScore = cross_val_score(nbrs, XS, y, cv=10, scoring='accuracy')
+fScore = cross_val_score(nbrs, XS, y, cv=10, scoring='f1')
+pScore = cross_val_score(nbrs, XS, y, cv=10, scoring='precision')
+rScore = cross_val_score(nbrs, XS, y, cv=10, scoring='recall')
+
+trainAScore = cross_val_score(nbrs, X_trainS, y_train, cv=4, scoring='accuracy')
+trainFScore = cross_val_score(nbrs, X_trainS, y_train, cv=4, scoring='f1')
+trainPScore = cross_val_score(nbrs, X_trainS, y_train, cv=10, scoring='precision')
+trainRScore = cross_val_score(nbrs, X_trainS, y_train, cv=10, scoring='recall')
 
 print(f'Average accuracy: {np.mean(aScore):.4f}')
 print(f'Average f1-score: {np.mean(fScore):.4f}')
 print(f'Average precision: {np.mean(pScore):.4f}')
 print(f'Average recall: {np.mean(rScore):.4f}\n')
 
-y_pred = nbrs.predict(X_test)
+print(f'Average accuracy (training set): {np.mean(trainAScore):.4f}')
+print(f'Average f1-score (training set): {np.mean(trainFScore):.4f}')
+print(f'Average precision (training set): {np.mean(trainPScore):.4f}')
+print(f'Average recall (training set): {np.mean(trainRScore):.4f}\n')
+
+
 cm = confusion_matrix(y_test, y_pred)
 cmDisp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=nbrs.classes_)
 cmDisp.plot(cmap=plt.cm.Blues)
